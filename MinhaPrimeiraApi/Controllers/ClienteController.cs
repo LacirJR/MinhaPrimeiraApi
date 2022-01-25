1cd5d51f10c09ec2fb10acb31654165931deb0bb
Lacir Junior <74772578+LacirJR@users.noreply.github.com>
2022-01-25 16:38:15 -0300

    Upload

---
+++ MinhaPrimeiraApi/Controllers/ClienteController.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MinhaPrimeiraApi.Controllers
+{
+
+    [ApiController]
+    public class ClienteController : ControllerBase
+    {
+        private readonly Conexoes.Sql _sql;
+
+        public  ClienteController()
+        {
+            _sql = new Conexoes.Sql();
+        }
+        
+
+        //INSERIR CLIENTE NA BD
+
+        [HttpPost("v1/Cliente")]
+        public void InserirCliente(Entidades.Cliente cliente)
+        {
+            _sql.InserirCliente(cliente);
+        }
+        
+        //ATUALIZAR CLIENTE
+
+        [HttpPut ("v1/ClienteAtualizar")]
+        public void AtualizarCliente(Entidades.Cliente cliente)
+        {
+            _sql.AtualizarCliente(cliente);
+        }
+
+        // VERIFICAR EXISTENCIA NA BD COM O CPF
+
+        [HttpGet("v1/ClienteVerificar/{cpf}")]
+        public bool VerificarExistencia(string cpf)
+        {
+             return _sql.VerificarExistenciaCliente(cpf);
+        }
+
+        // DELETAR DA BD
+
+        [HttpDelete("v1/ClienteDeletar")]
+        public void DeletarCliente(Entidades.Cliente cliente)
+        {
+            _sql.DeletarCliente(cliente);
+        }
+
+        // LISTAR CLIENTE
+
+        [HttpGet("v1/ClienteListar")]
+        public List<Entidades.Cliente> ListarClientes()
+        {
+           return _sql.ListarClientes();
+        }
+
+        // SELECIONAR CLIENTE
+
+        [HttpGet("v1/ClienteListar/{cpf}")]
+        public Entidades.Cliente Selecionar(string cpf)
+        {
+           return _sql.SelecionarCliente(cpf);
+        }
+
+    }
+}
